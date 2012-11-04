883135c4eebf61b9ad8a47fea69c3cb663925b0a
João Oliveira <ei11114@fe.up.pt>
2012-11-04 18:57:08 +0000

    git-svn-id: https://svn.fe.up.pt/repos/aedagja@52 4e2cfa59-03be-43de-8af8-bcaec2adcb25

--- src/Obra.h
+++ src/Obra.h
@@ -26,50 +26,189 @@ class Obra {
 	unsigned int nr;
 public:
 	Obra();
+	/*
+	 * @return Devolve o número de trabalhos na obra
+	 */
 	int getTamanho() const;
+	/*
+	 * @return Devolve o ultimo número de identificação atribuído a uma obra
+	 */
 	static unsigned int getUltimoNr();
+	/*
+	 * @brief Adiciona um trabalho à obra
+	 * @param Apontador para o trabalho a ser adicionado
+	 */
 	void adicionaTrabalho(Trabalho *t1);
+	/*
+	 * @brief Elimina um trabalho
+	 * @param Número do trabalho a ser eliminado
+	 */
 	bool eliminaTrab(unsigned int n);
+	/*
+	 * @return Devolve o numero da obra
+	 */
 	unsigned int getNr() const;
+	/*
+	 * @return Devolve o custo total da obra
+	 */
 	int getCustoTotal() const;
+	/*
+	 * @brief Calcula o custo total de todos os trabalhos de um determinado tipo
+	 * @param Tipo de trabalho
+	 * @return Custo total dos trabalhos
+	 */
 	int getCustoTrab(tipoTrabalho t) const;
-	int getDuracaoTrab(tipoTrabalho t) const;;
+	/*
+	 * @brief Calcula a duração total de todos os trabalhos de um determinado tipo
+	 * @param Tipo de trabalho
+	 * @return Duração total dos trabalhos
+	 */
+	int getDuracaoTrab(tipoTrabalho t) const;
+	/*
+	 * @return Devolve a duração total da obra
+	 */
 	int getDuracaoTotal() const;
+	/*
+	 * @return Devolve o asfalto total na obra
+	 */
 	int getAsfaltoTotal() const;
+	/*
+	 * @return Devolve o betão total na obra
+	 */
 	int getBetaoTotal() const;
+	/*
+	 * @return Devolve o cabo total na obra
+	 */
 	int getCaboTotal() const;
+	/*
+	 * @return Devolve a madeira total na obra
+	 */
 	int getMadeiraTotal() const;
+	/*
+	 * @return Devolve o trabalho mais barato
+	 */
 	Trabalho &trabalhoMaisBarato();
+	/*
+	 * @return Devolve o trabalho mais caro
+	 */
 	Trabalho &trabalhoMaisCaro();
+	/*
+	 * @return Devolve o trabalho com menor duração
+	 */
 	Trabalho &trabalhoMenorDuracao();
+	/*
+	 * @return Devolve o trabalho com maior duração
+	 */
 	Trabalho &trabalhoMaiorDuracao();
+	/*
+	 * @brief Procura um trabalho pelo seu número
+	 * @param Número do trabalho
+	 * @return Devolve o trabalho com o número correspondente
+	 */
 	string searchNum(int search);
+	/*
+	 * @param Valor do custo pretendido
+	 * @result Devolve os trabalhos com custo inferior ao introduzido
+	 */
 	vector<Trabalho *> trabalhosCustoMenor(int c) const;
+	/*
+	 * @param Valor da duração pretendida
+	 * @result Devolve os trabalhos com duração inferior à introduzida
+	 */
 	vector<Trabalho *> trabalhosDuracaoMenor(int d) const;
+	/*
+	 * @param Quandtidade de asfalto pretendida
+	 * @result Devolve os trabalhos com quantidade de asfalto inferior à introduzida
+	 */
 	vector<Trabalho *> trabalhosAsfaltoMenor(int a) const;
+	/*
+	 * @param Quandtidade de betão pretendida
+	 * @result Devolve os trabalhos com quantidade de betão inferior à introduzida
+	 */
 	vector<Trabalho *> trabalhosBetaoMenor(int b) const;
+	/*
+	 * @param Comprimento de cabo pretendido
+	 * @result Devolve os trabalhos com comprimento de cabo inferior ao introduzido
+	 */
 	vector<Trabalho *> trabalhosCaboMenor(int c) const;
+	/*
+	 * @param Área de madeira pretendida
+	 * @result Devolve os trabalhos com área de madeira inferior à introduzida
+	 */
 	vector<Trabalho *> trabalhosMadeiraMenor(int m) const;
+	/*
+	 * @param Valor do custo pretendido
+	 * @result Devolve os trabalhos com custo superior ao introduzido
+	 */
 	vector<Trabalho *> trabalhosCustoMaior(int c) const;
+	/*
+	 * @param Duração pretendida
+	 * @result Devolve os trabalhos com duração superior à introduzida
+	 */
 	vector<Trabalho *> trabalhosDuracaoMaior(int d) const;
+	/*
+	 * @param Quandtidade de asfalto pretendida
+	 * @result Devolve os trabalhos com quantidade de asfalto superior à introduzida
+	 */
 	vector<Trabalho *> trabalhosAsfaltoMaior(int a) const;
+	/*
+	 * @param Quandtidade de asfalto pretendida
+	 * @result Devolve os trabalhos com quantidade de betão superior à introduzida
+	 */
 	vector<Trabalho *> trabalhosBetaoMaior(int b) const;
+	/*
+	 * @param Comprimento de cabo pretendido
+	 * @result Devolve os trabalhos com comprimento de cabo superior ao introduzido
+	 */
 	vector<Trabalho *> trabalhosCaboMaior(int c) const;
+	/*
+	 * @param Área de madeira pretendida
+	 * @result Devolve os trabalhos com área de madeira superior à introduzida
+	 */
 	vector<Trabalho *> trabalhosMadeiraMaior(int m) const;
+	/*
+	 * @param Nome da empresa
+	 * @return Devolve os trabalhos que pertencem a uma empresa
+	 */
 	vector<Trabalho *> trabalhosEmpresa(string emp) const;
+	/*
+	 * @param ID da rua
+	 * @return Devolve todos os trabalhos a serem feitos numa determinada rua
+	 */
 	vector<Trabalho *> trabalhosRua(int id) const;
+	/*
+	 * @param ID da habitação
+	 * @return Devolve todos os trabalhos a serem feitos numa determinada habitação
+	 */
 	vector<Trabalho *> trabalhosHabitacao(int id) const;
+	/*
+	 * @param Tipo de trabalho
+	 * @return Devolve todos os trabalhos de um tipo
+	 */
 	vector<Trabalho *> trabalhosTipo(tipoTrabalho tp);
+	/*
+	 * @param Número do trabalho pretendido
+	 * @return Devolve o trabalho com um determinado número
+	 */
 	Trabalho* getTrabalho(unsigned int n);
+	/*
+	 *  @brief Imprime na consola informações sobre a obra
+	 */
 	void imprime() const;
+	/*
+	 *  @brief Guarda no ficheiro de escrita informações sobre a obra
+	 *  @param Ficheiro onde se pretende guardar as informações
+	 */
 	void imprimeFicheiro(ofstream& ficheiro_escrita) const;
 
 	class TrabalhoInexistente {
 	public:
 		int id;
-		TrabalhoInexistente(int id): id(id) {}
+		TrabalhoInexistente(int id) :
+				id(id) {
+		}
 	};
- };
+};
 
 class Construtora {
 	string nome;
@@ -119,7 +258,9 @@ public:
 	class ObraInexistente {
 	public:
 		int id;
-		ObraInexistente(int id): id(id) {}
+		ObraInexistente(int id) :
+				id(id) {
+		}
 	};
 
 };
--- src/Trabalho.h
+++ src/Trabalho.h
@@ -6,8 +6,9 @@
 
 using namespace std;
 
-
-enum tipoTrabalho{arruamento, saneamento, trolha, eletricista, carpinteiro};
+enum tipoTrabalho {
+	arruamento, saneamento, trolha, eletricista, carpinteiro
+};
 
 class Trabalho {
 	unsigned int duracao, custo;
@@ -15,12 +16,34 @@ class Trabalho {
 	static unsigned int ultimoNum;
 	unsigned int num;
 public:
+	/*
+	 * @brief Construtor da classe Trabalho
+	 * @param Duração, custo e empresa responsavél pelo trabalho
+	 */
 	Trabalho(int duracao, int custo, string empresa);
-	virtual ~Trabalho() {}
+	/*
+	 * @brief Destrutor da classe Trabalho
+	 */
+	virtual ~Trabalho() {
+	}
+	/*
+	 * @return Devolve o número do trabalho
+	 */
 	unsigned int getNum() const;
+	/*
+	 * @return Devolve a empresa responsavél pelo trabalho
+	 */
 	string getEmpresa() const;
-	/*virtual*/ int getCusto() const;
-	/*virtual*/ int getDuracao() const;
+	/*
+	 * @return Devolve o custo do trabalho
+	 */
+	/*virtual*/
+	int getCusto() const;
+	/*
+	 * @return Devolve a duração do trabalho
+	 */
+	/*virtual*/
+	int getDuracao() const;
 	virtual int getAsfalto() const = 0;
 	virtual int getBetao() const;
 	virtual int getCabo() const;
@@ -30,15 +53,33 @@ public:
 	virtual void imprimeFicheiro(ofstream& ficheiro_escrita) const =0;
 	virtual tipoTrabalho getTipoTrabalho() const =0;
 	virtual int getId() const =0;
+	/*
+	 * @brief Altera a empresa responsavél
+	 * @param Empresa pretendida
+	 */
 	void setEmpresa(string str);
+	/*
+	 * @brief Altera a duração do trabalho
+	 * @param Duração pretendida
+	 */
 	void setDuracao(unsigned int n);
+	/*
+	 * @brief Altera o custo do trabalho
+	 * @param Custo pretendido
+	 */
 	void setCusto(unsigned int n);
+	/*
+	 *  @brief Guarda no ficheiro de escrita informações sobre o trabalho
+	 *  @param Ficheiro onde se pretende guardar as informações
+	 */
 	virtual void imprime() const;
 
 	class ValorIncorrecto {
 	public:
 		int v;
-		ValorIncorrecto(int v): v(v) {}
+		ValorIncorrecto(int v) :
+				v(v) {
+		}
 	};
 };
 
