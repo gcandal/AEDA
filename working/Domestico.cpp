f6c71e6565f9508345df12833c80fe66c6b7c158
João Oliveira <ei11114@fe.up.pt>
2012-11-04 20:14:57 +0000

    git-svn-id: https://svn.fe.up.pt/repos/aedagja@57 4e2cfa59-03be-43de-8af8-bcaec2adcb25

---
+++ working/Domestico.cpp
@@ -0,0 +1,146 @@
+#include "Obra.h"
+#include "Domestico.h"
+
+//CLASSE DOMESTICO
+Domestico::Domestico(int duracao, int custo, string empresa, int idHabitacao) :
+		Trabalho(duracao, custo, empresa), idHabitacao(idHabitacao) {
+}
+
+int Domestico::getIdHabitacao() const {
+	return idHabitacao;
+}
+
+void Domestico::setID(unsigned int n) {
+	idHabitacao=n;
+}
+
+void Domestico::imprime() const {
+
+	Trabalho::imprime();
+	cout << "ID Habitacao: " << idHabitacao << endl;
+}
+
+//CLASSE TROLHA
+Trolha::Trolha(int duracao, int custo, string empresa, int idHabitacao,
+		int quantBetao) :
+		Domestico(duracao, custo, empresa, idHabitacao), quantBetao(quantBetao) {
+}
+
+int Trolha::getBetao() const {
+	return quantBetao;
+}
+
+int Trolha::getAsfalto() const {
+	return 0;
+}
+
+int Trolha::getCabo() const {
+	return 0;
+}
+
+int Trolha::getMadeira() const {
+	return 0;
+}
+
+void Trolha::setMaterial(unsigned int n) {
+	quantBetao=n;
+
+	if(n<0)
+		throw ValorIncorrecto(n);
+}
+
+void Trolha::imprimeFicheiro(ofstream& ficheiro_escrita) const {
+
+	ficheiro_escrita << Trabalho::getDuracao() << '\n' << Trabalho::getCusto() << '\n' << Domestico::getIdHabitacao() << '\n'
+			<< quantBetao << '\n' << "Trolha" << '\n' << Trabalho::getEmpresa() << '\n';
+}
+
+void Trolha::imprime() const {
+
+	Domestico::imprime();
+	cout << "Tipo de trabalho: Trolha" << ", " << "Quantidade de betao: " << quantBetao << endl;
+}
+
+//CLASSE ELETRICISTA
+Eletricista::Eletricista(int duracao, int custo, string empresa,
+		int idHabitacao, int compCabo) :
+		Domestico(duracao, custo, empresa, idHabitacao), compCabo(compCabo) {
+}
+
+int Eletricista::getCabo() const {
+	return compCabo;
+}
+
+int Eletricista::getBetao() const {
+	return 0;
+}
+
+int Eletricista::getAsfalto() const {
+	return 0;
+}
+
+int Eletricista::getMadeira() const {
+	return 0;
+}
+
+void Eletricista::setMaterial(unsigned int n) {
+	compCabo=n;
+
+	if(n<0)
+		throw ValorIncorrecto(n);
+}
+
+void Eletricista::imprimeFicheiro(ofstream& ficheiro_escrita) const {
+
+	ficheiro_escrita << Trabalho::getDuracao() << '\n' << Trabalho::getCusto() << '\n' << Domestico::getIdHabitacao() << '\n'
+			<< compCabo << '\n' << "Eletricista" << '\n' << Trabalho::getEmpresa() << '\n';
+}
+
+
+void Eletricista::imprime() const {
+
+	Domestico::imprime();
+	cout << "Tipo de trabalho: Eletricista" << ", " << "Quantidade de cabo: " << compCabo << endl;
+}
+
+//CLASSE CARPINTEIRO
+Carpinteiro::Carpinteiro(int duracao, int custo, string empresa,
+		int idHabitacao, unsigned int areaMadeira) :
+		Domestico(duracao, custo, empresa, idHabitacao), areaMadeira(
+				areaMadeira) {
+}
+
+int Carpinteiro::getCabo() const {
+	return 0;
+}
+
+int Carpinteiro::getBetao() const {
+	return 0;
+}
+
+int Carpinteiro::getAsfalto() const {
+	return 0;
+}
+
+int Carpinteiro::getMadeira() const {
+	return areaMadeira;
+}
+
+void Carpinteiro::setMaterial(unsigned int n) {
+	areaMadeira=n;
+
+	if(n<0)
+		throw ValorIncorrecto(n);
+}
+
+void Carpinteiro::imprimeFicheiro(ofstream& ficheiro_escrita) const {
+
+	ficheiro_escrita << Trabalho::getDuracao() << '\n' << Trabalho::getCusto() << '\n' << Domestico::getIdHabitacao() << '\n'
+			<< areaMadeira << '\n' << "Carpinteiro" << '\n' << Trabalho::getEmpresa() << '\n';
+}
+
+void Carpinteiro::imprime() const {
+
+	Domestico::imprime();
+	cout << "Tipo de trabalho: Carpinteiro" << ", " << "Area de madeira: " << areaMadeira << endl;
+}
---
+++ working/Domestico.h
@@ -0,0 +1,144 @@
+#ifndef DOMESTICO_H_
+#define DOMESTICO_H_
+#include <vector>
+#include <string>
+#include "Trabalho.h"
+
+using namespace std;
+
+class Domestico: public Trabalho {
+	unsigned int idHabitacao;
+public:
+	/*
+	 * @brief Construtor da classe Doméstico
+	 * @param Duração, custo, empresa responsável e id da habitação onde vai ser realizado o trabalho
+	 */
+	Domestico(int duracao, int custo, string empresa, int idHabitacao);
+	/*
+	 * @result Devolve o ID da habitação onde está a ser realizado o trabalho
+	 */
+	int getIdHabitacao() const;
+	/*
+	 * @brief Altera o ID da habitação
+	 * @param Novo ID
+	 */
+	void setID(unsigned int n);
+	/*
+	 * @brief Imprime informações sobre o trabalho
+	 */
+	virtual void imprime() const;
+};
+
+class Trolha: public Domestico {
+	unsigned int quantBetao;
+	const static tipoTrabalho tipo = trolha;
+public:
+	/*
+	 * @brief Construtor da classe Trolha
+	 * @param Duração, custo, empresa responsável, id da rua onde vai ser realizado o trabalho e quantidade de betão disponivel
+	 */
+	Trolha(int duracao, int custo, string empresa, int idRua, int quantBetao);
+	/*
+	 * @return Devolve a quantidade de betão disponivel
+	 */
+	int getBetao() const;
+	int getAsfalto() const;
+	int getCabo() const;
+	int getMadeira() const;
+	/*
+	 * @brief Altera a quantidade de material disponivel
+	 * @param Nova quantidade de material
+	 */
+	void setMaterial(unsigned int n);
+	/*
+	 * @brief Guarda no ficheiro .txt as informações do trabalho
+	 * @param Ficheiro de texto que vai receber as informações
+	 */
+	void imprimeFicheiro(ofstream& ficheiro_escrita) const;
+	/*
+	 * @return Devolve o tipo de trabalho
+	 */
+	virtual tipoTrabalho getTipoTrabalho() const {
+		return tipo;
+	}
+	/*
+	 * @brief Imprime informações sobre o trabalho
+	 */
+	void imprime() const;
+};
+
+class Eletricista: public Domestico {
+	unsigned int compCabo;
+	const static tipoTrabalho tipo = eletricista;
+public:
+	/*
+	 * @brief Construtor da classe Eletricista
+	 * @param Duração, custo, empresa responsável, id da rua onde vai ser realizado o trabalho e comprimento de cabo disponivel
+	 */
+	Eletricista(int duracao, int custo, string empresa, int idRua,
+			int compCabo);
+	/*
+	 * @return Devolve o comprimento de cabo disponivel
+	 */
+	int getCabo() const;
+	int getBetao() const;
+	int getAsfalto() const;
+	int getMadeira() const;
+	/*
+	 * @brief Altera a quantidade de material disponivel
+	 * @param Nova quantidade de material
+	 */
+	void setMaterial(unsigned int n);
+	/*
+	 * @brief Guarda no ficheiro .txt as informações do trabalho
+	 * @param Ficheiro de texto que vai receber as informações
+	 */
+	void imprimeFicheiro(ofstream& ficheiro_escrita) const;
+	/*
+	 * @return Devolve o tipo de trabalho
+	 */
+	virtual tipoTrabalho getTipoTrabalho() const {
+		return tipo;
+	}
+	/*
+	 * @brief Imprime informações sobre o trabalho
+	 */
+	void imprime() const;
+};
+
+class Carpinteiro: public Domestico {
+	unsigned int areaMadeira;
+	const static tipoTrabalho tipo = carpinteiro;
+public:
+	Carpinteiro(int duracao, int custo, string empresa, int idRua,
+			unsigned int areaMadeira);
+	int getCabo() const;
+	int getBetao() const;
+	int getAsfalto() const;
+	/*
+	 * @return Devolve a área de madeira disponivel
+	 */
+	int getMadeira() const;
+	/*
+	 * @brief Altera a quantidade de material disponivel
+	 * @param Nova quantidade de material
+	 */
+	void setMaterial(unsigned int n);
+	/*
+	 * @brief Guarda no ficheiro .txt as informações do trabalho
+	 * @param Ficheiro de texto que vai receber as informações
+	 */
+	void imprimeFicheiro(ofstream& ficheiro_escrita) const;
+	/*
+	 * @return Devolve o tipo de trabalho
+	 */
+	virtual tipoTrabalho getTipoTrabalho() const {
+		return tipo;
+	}
+	/*
+	 * @brief Imprime informações sobre o trabalho
+	 */
+	void imprime() const;
+};
+
+#endif /* DOMESTICO_H_ */
