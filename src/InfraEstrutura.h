7aa10a62fdbd7603adf3c64e04038afc9e22cab6
Ana Sousa <ei11068@gmail.com>
2012-11-04 23:39:22 +0000

    git-svn-id: https://svn.fe.up.pt/repos/aedagja@66 4e2cfa59-03be-43de-8af8-bcaec2adcb25

--- src/InfraEstrutura.h
+++ src/InfraEstrutura.h
@@ -10,28 +10,28 @@ class InfraEstrutura: public Trabalho {
 private:
 	unsigned int idRua;
 public:
-	/*
-	 * @brief Construtor da classe InfraEstrutura
-	 * @param Duração, custo, empresa responsável e id da habitação onde vai ser realizado o trabalho
+	/*!
+	 * /brief Construtor da classe InfraEstrutura
+	 * /param Duração, custo, empresa responsável e id da habitação onde vai ser realizado o trabalho
 	 */
 	InfraEstrutura(int duracao, int custo, string empresa, int idRua);
-	/*
-	 * @result Devolve o ID da rua onde está a ser realizado o trabalho
+	/*!
+	 * /result Devolve o ID da rua onde está a ser realizado o trabalho
 	 */
 	int getIdRua() const;
-	/*
-	 * @brief Altera o ID da rua
-	 * @param Novo ID
+	/*!
+	 * /brief Altera o ID da rua
+	 * /param Novo ID
 	 */
 	void setID(unsigned int n);
-	/*
-	 * @result Devolve o ID da rua onde está a ser realizado o trabalho
+	/*!
+	 * /result Devolve o ID da rua onde está a ser realizado o trabalho
 	 */
 	virtual int getId() const {
 		return idRua;
 	}
-	/*
-	 * @brief Imprime na consola informações sobre o trabalho
+	/*!
+	 * /brief Imprime na consola informações sobre o trabalho
 	 */
 	virtual void imprime() const;
 };
@@ -40,37 +40,37 @@ class Arruamento: public InfraEstrutura {
 	unsigned int quantAsfalto;
 	const static tipoTrabalho tipo = arruamento;
 public:
-	/*
-	 * @brief Construtor da classe Arruamento
-	 * @param Duração, custo, empresa responsável, id da rua onde vai ser realizado o trabalho e quantidade de asfalto disponivel
+	/*!
+	 * /brief Construtor da classe Arruamento
+	 * /param Duração, custo, empresa responsável, id da rua onde vai ser realizado o trabalho e quantidade de asfalto disponivel
 	 */
 	Arruamento(int duracao, int custo, string empresa, int idRua,
 			int quantAsfalto);
-	/*
-	 * @return Devolve a quantidade de asfalto disponivel
+	/*!
+	 * /return Devolve a quantidade de asfalto disponivel
 	 */
 	int getAsfalto() const;
 	int getBetao() const;
 	int getCabo() const;
 	int getMadeira() const;
-	/*
-	 * @brief Altera a quantidade de material disponivel
-	 * @param Nova quantidade de material
+	/*!
+	 * /brief Altera a quantidade de material disponivel
+	 * /param Nova quantidade de material
 	 */
 	void setMaterial(unsigned int n);
-	/*
-	 * @brief Guarda no ficheiro .txt as informações do trabalho
-	 * @param Ficheiro de texto que vai receber as informações
+	/*!
+	 * /brief Guarda no ficheiro .txt as informações do trabalho
+	 * /param Ficheiro de texto que vai receber as informações
 	 */
 	void imprimeFicheiro(ofstream& ficheiro_escrita) const;
-	/*
-	 * @return Devolve o tipo de trabalho
+	/*!
+	 * /return Devolve o tipo de trabalho
 	 */
 	tipoTrabalho getTipoTrabalho() const {
 		return tipo;
 	}
-	/*
-	 * @brief Imprime na consola informações sobre o trabalho
+	/*!
+	 * /brief Imprime na consola informações sobre o trabalho
 	 */
 	void imprime() const;
 };
@@ -79,37 +79,37 @@ class Saneamento: public InfraEstrutura {
 	unsigned int quantBetao;
 	const static tipoTrabalho tipo = saneamento;
 public:
-	/*
-	 * @brief Construtor da classe Saneamento
-	 * @param Duração, custo, empresa responsável, id da rua onde vai ser realizado o trabalho e quantidade de betão disponivel
+	/*!
+	 * /brief Construtor da classe Saneamento
+	 * /param Duração, custo, empresa responsável, id da rua onde vai ser realizado o trabalho e quantidade de betão disponivel
 	 */
 	Saneamento(int duracao, int custo, string empresa, int idRua,
 			int quantBetao);
-	/*
-	 * @return Devolve a quantidade de betão disponivel
+	/*!
+	 * /return Devolve a quantidade de betão disponivel
 	 */
 	int getBetao() const;
 	int getAsfalto() const;
 	int getCabo() const;
 	int getMadeira() const;
-	/*
-	 * @brief Altera a quantidade de material disponivel
-	 * @param Nova quantidade de material
+	/*!
+	 * /brief Altera a quantidade de material disponivel
+	 * /param Nova quantidade de material
 	 */
 	void setMaterial(unsigned int n);
-	/*
-	 * @brief Guarda no ficheiro .txt as informações do trabalho
-	 * @param Ficheiro de texto que vai receber as informações
+	/*!
+	 * /brief Guarda no ficheiro .txt as informações do trabalho
+	 * /param Ficheiro de texto que vai receber as informações
 	 */
 	void imprimeFicheiro(ofstream& ficheiro_escrita) const;
-	/*
-	 * @return Devolve o tipo de trabalho
+	/*!
+	 * /return Devolve o tipo de trabalho
 	 */
 	tipoTrabalho getTipoTrabalho() const {
 		return tipo;
 	}
-	/*
-	 * @brief Imprime na consola informações sobre o trabalho
+	/*!
+	 * /brief Imprime na consola informações sobre o trabalho
 	 */
 	void imprime() const;
 };
